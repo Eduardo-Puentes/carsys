6a88a03be8d801aaaede88f3a061dd1dd19e99b9
eduardopuentescs <eduardo@codestacksystems.com>
2023-11-28 09:13:58 -0600

    Base

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,19 +12,11 @@ const Home = () => {
 
     async function getRecords() {
         try {
-            const response = await fetch(`https://api.thingspeak.com/channels/2354453/fields/1.json?api_key=4FZGVC9KG39BNTEZ&results=2`);
             const responseTemp = await fetch(`https://api.thingspeak.com/channels/2311047/fields/1.json?api_key=LJLFEUXM4RY0RF47&results=2`);
             const responseHum = await fetch(`https://api.thingspeak.com/channels/2311047/fields/2.json?api_key=LJLFEUXM4RY0RF47&results=2`);
             const responseDis = await fetch(`https://api.thingspeak.com/channels/2311047/fields/3.json?api_key=LJLFEUXM4RY0RF47&results=2`);
             const responseCal = await fetch(`https://api.thingspeak.com/channels/2311047/fields/4.json?api_key=LJLFEUXM4RY0RF47&results=2`);
-
-            if (!response.ok && !responseTemp.ok && !responseHum.ok && !responseCal.ok) {
-                const message = 'An error occurred';
-                window.alert(message);
-                return;
-            }
     
-            const records = await response.json();
             const recordsTemp = await responseTemp.json();
             const recordsHum = await responseHum.json();
             const recordsDis = await responseDis.json();
@@ -64,6 +56,21 @@ const Home = () => {
       }, [records])
 
     useEffect(() => {
+        async function getAct() {
+            const response = await fetch(`https://api.thingspeak.com/channels/2354453/fields/1.json?api_key=4FZGVC9KG39BNTEZ&results=2`);
+
+            if (!response.ok) {
+                const message = `An error occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+
+            const records = await response.json();
+            console.log(records.feeds[1].field1);
+            setActive(records.feeds[1].field1);
+
+        }
+        getAct();
         getRecords();
     }, [])
 
