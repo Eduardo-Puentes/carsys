bef37446375c18901feaabea4098092666b4ca82
eduardopuentescs <eduardo@codestacksystems.com>
2023-11-28 09:02:58 -0600

    Base

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import alarm from './triangle-exclamation-solid.svg';
 import './style.css';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     const [active, setActive] = useState(0);
     const [temperatura, setTemperatura] = useState(0);
     const [calidad, setCalidad] = useState(0);
+    const [distancia, setDistancia] = useState(0);
     const [humedad, setHumedad] = useState(0);
 
     async function getRecords() {
@@ -13,6 +15,7 @@ const Home = () => {
             const response = await fetch(`https://api.thingspeak.com/channels/2354453/fields/1.json?api_key=4FZGVC9KG39BNTEZ&results=2`);
             const responseTemp = await fetch(`https://api.thingspeak.com/channels/2311047/fields/1.json?api_key=LJLFEUXM4RY0RF47&results=2`);
             const responseHum = await fetch(`https://api.thingspeak.com/channels/2311047/fields/2.json?api_key=LJLFEUXM4RY0RF47&results=2`);
+            const responseDis = await fetch(`https://api.thingspeak.com/channels/2311047/fields/3.json?api_key=LJLFEUXM4RY0RF47&results=2`);
             const responseCal = await fetch(`https://api.thingspeak.com/channels/2311047/fields/4.json?api_key=LJLFEUXM4RY0RF47&results=2`);
 
             if (!response.ok && !responseTemp.ok && !responseHum.ok && !responseCal.ok) {
@@ -24,14 +27,17 @@ const Home = () => {
             const records = await response.json();
             const recordsTemp = await responseTemp.json();
             const recordsHum = await responseHum.json();
+            const recordsDis = await responseDis.json();
             const recordsCal = await responseCal.json();
             console.log(records.feeds[1].field1);
             console.log(recordsTemp.feeds[1].field1);
             console.log(recordsHum.feeds[1].field2);
+            console.log(recordsDis.feeds[1].field3);
             console.log(recordsCal.feeds[1].field4);
             setActive(records.feeds[1].field1);
             setRecords(records);
             setTemperatura(recordsTemp.feeds[1].field1);
+            setDistancia(recordsDis.feeds[1].field3);
             setCalidad(recordsCal.feeds[1].field4);
             setHumedad(recordsHum.feeds[1].field2);
         }
@@ -50,7 +56,7 @@ const Home = () => {
             setRecords(itemsa)
           }
         })
-      }, 2000)
+      }, 1000)
       return () => {
         clearInterval(intervalId);
         mounted = false;
@@ -121,6 +127,13 @@ return (
                 <p>{humedad}%</p>
             </div>
         </div>
+
+        {((distancia > 5) && (active === "1")) && <div>
+            <div className='alerta'>
+                <p>ALARMA!</p>
+                <img className='alerta-icon' src={alarm} alt='alarm' />
+            </div>
+        </div>}
         </div>
 
             
